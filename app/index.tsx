2834af885544a6d978f9e5070e4c8ee20159f596
Losugan Sivasuthan <91836455+sugan1605@users.noreply.github.com>
2025-12-01 07:54:31 +0100

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,3 @@
-// app/index.tsx
 import { useEffect, useRef } from "react";
 import { View, Text, Animated, Easing, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
